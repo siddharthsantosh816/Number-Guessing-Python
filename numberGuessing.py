717072acead2dbc24bfe82755e670963284ff1fb
Siddharth Santosh <79411827+siddharthsantosh816@users.noreply.github.com>
2022-01-30 16:52:21 +0530

    Update numberGuessing.py

--- numberGuessing.py
+++ numberGuessing.py
@@ -1,23 +1,19 @@
 import random
 
 number=random.randint(1,10)
-print(number)
 
 
-
-for chance in range(1,4):
-    print(chance)
-    enter=int(input("Guess a number between 1 and 10. You have 3 chances."))
+for chance in range(3,0, -1):
+    print (chance,  "Number of Chances left: ")
+    enter=int(input("Guess a number between 1 and 10: "))
 
     if enter==number:
         print("You have guessed it correctly!")
+        break
 
     elif enter != number and chance<4:
         print("Wrong guess!,try once more")
      
 
-    else:   
-        print("Try again next time. All your chances are over.")
-
-
-
+else:   
+    print("Try again next time. All your chances are over.")
